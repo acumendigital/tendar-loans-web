d20e42ac81c56af045e0592e8a544ce76bad52a1
David Emaye <emayeodavid@gmail.com>
2023-11-27 23:18:25 +0100

    developed card component

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,8 +3,44 @@
     <div>
       <div class="title">Welcome Josh</div>
     </div>
+    <div class="cards_section">
+      <DashboardCard v-for="(data, index) in cardData" :key="index" :cardLength="index" :cardData="data" />
+    </div>
     <TableTransactions />
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  data () {
+    return {
+      cardData: [
+        {
+          title: 'Wallet balance',
+          text: '₦17,250.00',
+          date: '',
+          active: '',
+        },
+        {
+          title: 'Next instalment due',
+          text: '₦15,350.00',
+          date: 'Aug 30, 2023',
+          active: '',
+        },
+        {
+          title: 'Number of payments',
+          text: '1 of 4',
+          date: '',
+          active: 'Active loan',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cards_section {
+  display: flex;
+}
+</style>
