c229d0b4698490ee51d9c69501c42e52f15061bf
David Emaye <emayeodavid@gmail.com>
2023-12-05 13:10:47 +0100

    integrated login and logout

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -15,77 +15,71 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      cardData: [
-        {
-          title: "Wallet balance",
-          text: "₦17,250.00",
-          date: "",
-          active: "",
-        },
-        {
-          title: "Next instalment due",
-          text: "₦15,350.00",
-          date: "Aug 30, 2023",
-          active: "",
-        },
-        {
-          title: "Number of payments",
-          text: "1 of 4",
-          date: "",
-          active: "Active loan",
-        },
-      ],
-      tableData: [
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Successful",
-        },
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Unsuccessful",
-        },
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Successful",
-        },
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Unsuccessful",
-        },
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Successful",
-        },
-        {
-          reference: "BN-B1E73DA–0017",
-          transaction_type: "Wallet top-up",
-          amount: "30000",
-          date: "Dec 30, 09:42 PM",
-          transaction_status: "Successful",
-        },
-      ],
-    };
+<script setup>
+const cardData = ref([
+  {
+    title: "Wallet balance",
+    text: "₦17,250.00",
+    date: "",
+    active: "",
   },
-};
+  {
+    title: "Next instalment due",
+    text: "₦15,350.00",
+    date: "Aug 30, 2023",
+    active: "",
+  },
+  {
+    title: "Number of payments",
+    text: "1 of 4",
+    date: "",
+    active: "Active loan",
+  },
+]);
+const tableData = ref([
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Successful",
+  },
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Unsuccessful",
+  },
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Successful",
+  },
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Unsuccessful",
+  },
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Successful",
+  },
+  {
+    reference: "BN-B1E73DA–0017",
+    transaction_type: "Wallet top-up",
+    amount: "30000",
+    date: "Dec 30, 09:42 PM",
+    transaction_status: "Successful",
+  },
+]);
 </script>
 
 <style scoped>
