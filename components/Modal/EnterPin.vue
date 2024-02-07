e822450ae6afd9eb7e034c06aedc48495f2d3924
David Emaye <emayeodavid@gmail.com>
2024-02-07 12:31:17 +0100

    fixed enterpin modal width

--- components/Modal/EnterPin.vue
+++ components/Modal/EnterPin.vue
@@ -86,8 +86,8 @@ const createPin = () => {
   /* margin-top: 5%; */
   position: relative;
   background-color: white;
-  max-width: 550px;
-  width: 30%;
+  max-width: 600px;
+  width: 35%;
   height: fit-content;
   align-items: center;
   border-radius: 20px;
